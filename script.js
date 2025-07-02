01a2ec17e4407e57843f75babd5abdd6c765752e
Earl Syx Hernandez <hernandezearlsyxmail@gmail.com>
2025-07-03 06:23:16 +0800

    Handling an Esc Keypress event, classList, contains

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ const btnsOpenModal = document.querySelectorAll('.show-modal');
 console.log(btnsOpenModal);
 
 const openModal = function () {
-  console.log('Button Click');
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -18,7 +17,19 @@ const closeModal = function () {
 };
 
 for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', )''
+  btnsOpenModal[i].addEventListener('click', openModal);
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+
+/*global evenets - don't happen on once speific element,
+so listen to whole */
+
+document.addEventListener('keydown', function (e) {
+  /*e,  hey javascript call this function when a key down event happens, then please pass in the evnet object as argument */
+  console.log(e.key);
+
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
